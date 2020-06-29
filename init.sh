df74948a3b190259d8723dbaa5ad20f7c0cfcf5f
J.R. Maingat <jrmaingat@gmail.com>
2020-06-29 18:24:13 -0400

    Remove shell scripts

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "" > log.txt
-for i in {0..9}
-do
-    go run src/main.go server 4040,4041,4042,4043,4044 $i &>> "log.txt" &
-done
-go run src/main.go client 4040,4041,4042,4043,4044 localhost
-./kill.sh
